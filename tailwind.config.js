8d22e89be31f6f33dc474a29dbdf773e2899c8bc
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-08-25 13:26:29 +0100

    Update: add colors object to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        SoftRed: "hsl(7, 99%, 70%)",
+        Yellow: "hsl(51, 100%, 49%)",
+        DarkCyan: "hsl(167, 40%, 24%)",
+        DarkBlue: "hsl(198, 62%, 26%)",
+        moderateCyan: "hsl(168, 34%, 41%)",
+        White: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
-}
+};
